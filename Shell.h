05f2dae2a30342be03397bf726e4a74b1106e010
Nebenmor <nebenmor.anthony@gmail.com>
2023-10-18 08:10:46 +0100

    update 1

--- Shell.h
+++ Shell.h
@@ -3,13 +3,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+
 
 #define delim " \t\r\n\a\""
 #define MAX_INPUT_SIZE 1024
