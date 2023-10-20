7eeb533fd5df13691398bb82756dbf6b579306c5
Nebenmor <nebenmor.anthony@gmail.com>
2023-10-20 20:56:33 +0100

    update 1

--- exec_line.c
+++ exec_line.c
@@ -10,16 +10,16 @@ int Exec__line(data__shell *node)
 {
 	int (*builtin)(data__shell *node);
 
-	switch (node->args[0] != NULL) {
-		case 1:
+	switch (node->args[0] != NULL)
+	{case 1:
 			builtin = get_builtin(node->args[0]);
-			switch (builtin != NULL) {
-				case 1:
+			switch (builtin != NULL)
+			{case 1:
 					return (builtin(node));
 				default:
 					return (cmd_exec(node));
 			}
 		default:
-			return 1;
+			return (1);
 	}
 }
--- exit_shell.c
+++ exit_shell.c
@@ -20,17 +20,17 @@ int temp(data__shell *node)
 		str_len = _strlen(node->args[1]);
 		big_number = ustatus > (unsigned int)INT_MAX;
 
-		switch (1) {
-			case 1:
+		switch (1)
+		{ case 1:
 				if (!is_digit || str_len > 10 || big_number)
 				{
 					get_error(node, 2);
 					node->status = 2;
-					return 1;
+					return (1);
 				}
 				node->status = (ustatus % 256);
 				break;
 		}
 	}
-	return 0;
+	return (0);
 }
